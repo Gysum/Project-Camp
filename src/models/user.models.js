58978f06a6368130387285c0265069926d97265e
Gysum <aakashpathrikar@gmail.com>
2026-07-06 16:49:49 +0530

    setup user tokens

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,5 +1,7 @@
 import mongoose, {Schema} from "mongoose";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
 
 const userSchema = new Schema(
     {
@@ -72,5 +74,42 @@ userSchema.methods.isPasswordCorrect = async function(password) {
     return await bcrypt.compare(password, this.password);
 }
 
+
+userSchema.methods.generateAccessToken = async function() {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username
+        },
+        process.env.ACCESS_SECRET_KEY,
+        {expiresIn: process.env.ACCESS_SECRET_EXPIRY}
+    )
+}
+
+userSchema.methods.generateRefreshToken = async function() {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {expiresIn: process.env.REFRESH_TOKEN_EXPIRY}
+    )
+}
+
+userSchema.methods.generateTemporaryToken= async function () {
+    const unHashedToken = crypto.randomBytes(20).toString("hex")
+
+    const hashedToken = crypto
+        .createHash("sha256")
+        .update(unHashedToken)
+        .digest("hex")
+
+    const tokenExpiry = Date.now() + (20*60*1000)
+
+    return {unHashedToken, hashedToken, tokenExpiry}
+}
 
 export const User = mongoose.model("User", userSchema)
